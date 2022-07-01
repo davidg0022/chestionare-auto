517b343f08896ad2155000a22bcf651f0448181a
Gatea David <davidgatea21@gmail.com>
2022-07-01 15:59:27 +0300

    first

--- app.py
+++ app.py
@@ -13,66 +13,19 @@ port = int(os.environ.get('PORT', 5000))
 def get_data():
     files = [f for f in os.listdir('.') if os.path.isfile(f) and ".json" in f]
     data = []
-    # for f in files:
-    with open(files[0],encoding='utf-8') as json_file:
-        temp = json.load(json_file)
-        for q in temp:
-            data.append(q)
-    # shuffle(data)
+    for f in files:
+        with open(f,encoding='utf-8') as json_file:
+            temp = json.load(json_file)
+            for q in temp:
+                data.append(q)
+    shuffle(data)
     return data
-
-data = [
-  {
-    "corect": False,
-    "incorect": False,
-    "disabled": False,
-    "title": "În cazul unui accident în care victima şi-a pierdut cunoştinţa, prima măsură va fi:",
-    "answers": [
-      {
-        "text": "să administraţi medicamente din trusa de prim ajutor;",
-        "correct": "0",
-        "selected": False
-      },
-      {
-        "text": "să imobilizaţi eventualele fracturi;",
-        "correct": "0",
-        "selected": False
-      },
-      {
-        "text": "să controlaţi respiraţia şi bătăile inimii.",
-        "correct": "1",
-        "selected": False
-      }
-    ],
-    "img_url": ""
-  },
-  {
-    "corect": False,
-    "incorect": False,
-    "disabled": False,
-    "title": "Hemoragia nazală se poate opri dacă accidentatul:",
-    "answers": [
-      {
-        "text": "stă culcat cu capul pe spate;",
-        "correct": "0",
-        "selected": False
-      },
-      {
-        "text": "stă în picioare şi îşi strânge ambele nări timp de aproximativ 5-10 minute;",
-        "correct": "1",
-        "selected": False
-      },
-      {"text": "ridică mâna dreaptă.", "correct": "0", "selected": False}
-    ],
-    "img_url": ""
-  }]
+data = get_data()
 question = data[0]
 json_string = json.dumps(question,ensure_ascii=False)
 with open('current.json', 'w', encoding="utf-8") as outfile:
     outfile.write(json_string)
-i = 0
 
-    
 app = Flask(__name__)
 
 @app.route("/data_json", methods = ["GET", "POST"])
@@ -95,20 +48,27 @@ def select():
 
 @app.route("/next", methods = ["POST"])
 def next():
-    global question
+    with open("current.json",encoding='utf-8') as json_file:
+        question = json.load(json_file)
+
     if question["disabled"] != True:
         return redirect("/")
+
     question["corect"] = question["incorect"] = question["disabled"] = False
     question["answers"][0]["selected"] = question["answers"][1]["selected"] = question["answers"][2]["selected"] = False
     question = choice(data)
     while len(question["answers"]) != 3:
         question = choice(data)
- 
+        
+    json_string = json.dumps(question,ensure_ascii=False)
+    with open('current.json', 'w', encoding="utf-8") as outfile:
+        outfile.write(json_string)
 @app.route("/check_ans", methods = ["GET", "POST"])
 def check_ans():
+    with open("current.json",encoding='utf-8') as json_file:
+        question = json.load(json_file)
     if question["disabled"] == True:
         return redirect("/")
-     
     ok = False 
     for t in range(3):
         if question["answers"][t]["selected"] == True:
@@ -120,9 +80,17 @@ def check_ans():
     for t in range(3):
         if question["answers"][t]["selected"] == True and question["answers"][t]["correct"] == "0" or question["answers"][t]["selected"] == False and question["answers"][t]["correct"] == "1" :
             question["incorect"] = True
+            json_string = json.dumps(question,ensure_ascii=False)
+            with open('current.json', 'w', encoding="utf-8") as outfile:
+                outfile.write(json_string)
             return redirect("/")
     question["corect"] = True
-    
+
+
+    json_string = json.dumps(question,ensure_ascii=False)
+    with open('current.json', 'w', encoding="utf-8") as outfile:
+        outfile.write(json_string)
+
     return redirect("/")
 
 @app.route("/", methods = ["GET", "POST"])
